02f6b70bc6a138d688477685cf2f328a900d9de0
BreimAlaa <breim.alaa@gmail.com>
2023-08-28 19:24:13 +0300

    Created: CryptoManager blueprint with basic signing functionality

---
+++ CryptoManager.php
@@ -0,0 +1,56 @@
+<?php
+class CryptoManager
+{
+    public function hash($data): string
+    {
+        return hash('sha3-512', $data);
+    }
+
+    public function base64Encode($data): string
+    {
+        return base64_encode($data);
+    }
+
+    public function base64Decode($data): string
+    {
+        return base64_decode($data);
+    }
+
+    public function generateKeyPair(): array
+    {
+        $config = array(
+            "digest_alg" => "sha512",
+            "private_key_bits" => 4096,
+            "private_key_type" => OPENSSL_KEYTYPE_RSA,
+        );
+        $res = openssl_pkey_new($config);
+        openssl_pkey_export($res, $privateKey);
+        $publicKey = openssl_pkey_get_details($res);
+        $publicKey = $publicKey["key"];
+        return array(
+            "privateKey" => $privateKey,
+            "publicKey" => $publicKey
+        );
+    }
+
+    public function sign($data, $privateKey): string
+    {
+        openssl_sign($data, $signature, $privateKey, OPENSSL_ALGO_SHA512);
+        return $signature;
+    }
+    public function verify($data, $signature, $publicKey): bool
+    {
+        return openssl_verify($data, $signature, $publicKey, OPENSSL_ALGO_SHA512) === 1;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function __callStatic($name, $arguments): mixed
+    {
+        if (method_exists(CryptoManager::class, $name)) {
+            return call_user_func_array([CryptoManager::class, $name], $arguments);
+        }
+        throw new Exception("Method $name does not exist.");
+    }
+}
