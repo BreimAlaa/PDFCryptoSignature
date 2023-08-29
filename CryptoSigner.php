0ebf426029c960d159104eb9e51be51a7ba51f50
BreimAlaa <breim.alaa@gmail.com>
2023-08-30 01:56:51 +0300

    Added: keys for signing and verifying

--- CryptoSigner.php
+++ CryptoSigner.php
@@ -2,7 +2,7 @@
 interface CryptoSigner{
 //    public function load(string $path) : CryptoSigner;
     public static function load(string $path) : CryptoSigner;
-    public function sign(array $signerInfo);
-    public function verify() : array;
+    public function sign(string $privateKey, array $signerInfo) : void;
+    public function verify(string $publicKey) : array;
     public function path() : string;
 }
