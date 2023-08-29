d922d70b03016aa310ab87f577ab3fa9c8765740
BreimAlaa <breim.alaa@gmail.com>
2023-08-29 09:30:33 +0300

    Added: Static call for CryptoManager with IDE support

--- CryptoManager.php
+++ CryptoManager.php
@@ -1,22 +1,98 @@
 <?php
+
+/**
+ * @method static string hash(string $data)
+ * @params string $data
+ * @return string
+ *
+ * @method static string base64Encode(string $data)
+ * @params string $data
+ * @return string
+ *
+ * @method static string base64Decode(string $data)
+ * @params string $data
+ * @return string
+ *
+ * @method static array generateKeyPair() Generate public and private key pair
+ * @return array
+ *
+ * @method static string sign(string $data, string $privateKey) Sign the data with private key
+ * @params string $data
+ * @params string $privateKey
+ * @return string
+ *
+ * @method static bool verify(string $data, string $signature, string $publicKey) Verify the signature of data using the public key
+ * @params string $data
+ * @params string $signature
+ * @params string $privateKey
+ * @return bool
+ * 
+ * @method static string encrypt(string $data, string $privateKey) Encrypt data with private key
+ * @params string $data
+ * @params string $privateKey
+ * @return string
+ * 
+ * @method static string decrypt(string $data, string $privateKey) Decrypt data with private key
+ * @params string $data
+ * @params string $privateKey
+ * @return string
+ *
+ *
+ * @method string hash(string $data)
+ * @params string $data
+ * @return string
+ *
+ * @method string base64Encode(string $data)
+ * @params string $data
+ * @return string
+ *
+ * @method string base64Decode(string $data)
+ * @params string $data
+ * @return string
+ *
+ * @method array generateKeyPair() Generate public and private key pair
+ * @return array
+ *
+ * @method string sign(string $data, string $privateKey) Sign the data with private key
+ * @params string $data
+ * @params string $privateKey
+ * @return string
+ *
+ * @method bool verify(string $data, string $signature, string $publicKey) Verify the signature of data using the public key
+ * @params string $data
+ * @params string $signature
+ * @params string $privateKey
+ * @return bool
+ *
+ * @method string encrypt(string $data, string $privateKey) Encrypt data with private key
+ * @params string $data
+ * @params string $privateKey
+ * @return string
+ *
+ * @method string decrypt(string $data, string $privateKey) Decrypt data with private key
+ * @params string $data
+ * @params string $privateKey
+ * @return string
+ */
+
 class CryptoManager
 {
-    public function hash($data): string
+    private function hashForAlaa($data): string
     {
         return hash('sha3-512', $data);
     }
 
-    public function base64Encode($data): string
+    private function base64EncodeForAlaa($data): string
     {
         return base64_encode($data);
     }
 
-    public function base64Decode($data): string
+    private function base64DecodeForAlaa($data): string
     {
         return base64_decode($data);
     }
 
-    public function generateKeyPair(): array
+    private function generateKeyPairForAlaa(): array
     {
         $config = array(
             "digest_alg" => "sha512",
@@ -25,42 +101,45 @@ class CryptoManager
         );
         $res = openssl_pkey_new($config);
         openssl_pkey_export($res, $privateKey);
-        $publicKey = openssl_pkey_get_details($res);
-        $publicKey = $publicKey["key"];
+        $privateKey = openssl_pkey_get_details($res);
+        $privateKey = $privateKey["key"];
         return array(
             "privateKey" => $privateKey,
-            "publicKey" => $publicKey
+            "privateKey" => $privateKey
         );
     }
 
-    public function sign($data, $privateKey): string
+    private function signForAlaa($data, $privateKey): string
     {
         openssl_sign($data, $signature, $privateKey, OPENSSL_ALGO_SHA512);
         return $signature;
     }
-    public function verify($data, $signature, $publicKey): bool
+    private function verifyForAlaa($data, $signature, $publicKey): bool
     {
         return openssl_verify($data, $signature, $publicKey, OPENSSL_ALGO_SHA512) === 1;
     }
 
-    public function encrypt($data, $publicKey): string
+    private function encryptForAlaa($data, $privateKey): string
     {
-        openssl_public_encrypt($data, $encrypted, $publicKey);
+        openssl_private_encrypt($data, $encrypted, $privateKey);
         return $encrypted;
     }
-    public function decrypt($data, $privateKey): string
+    private function decryptForAlaa($data, $privateKey): string
     {
-        openssl_private_decrypt($data, $decrypted, $privateKey);
+        openssl_public_decrypt($data, $decrypted, $privateKey);
         return $decrypted;
     }
-    /**
-     * @throws Exception
-     */
-    public static function __callStatic($name, $arguments): mixed
+
+    public static function __callStatic(string $name, array $arguments)
     {
-        if (method_exists(CryptoManager::class, $name)) {
-            return call_user_func_array([CryptoManager::class, $name], $arguments);
-        }
-        throw new Exception("Method $name does not exist.");
+        $instance = new static();
+        $method = $name . 'ForAlaa';
+        return $instance->$method(...$arguments);
+    }
+
+    public function __call(string $name, array $arguments)
+    {
+        $method = $name . 'ForAlaa';
+        return $this->$method(...$arguments);
     }
 }
